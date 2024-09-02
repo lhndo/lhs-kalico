cce269188c56a7ad7c08d0540385dc90f7650d78
undoz <andrei@undoz.com>
2024-09-02 03:05:53 +0000

    merging upstream diff

--- klippy/extras/temperature_mcu.py
+++ klippy/extras/temperature_mcu.py
@@ -89,9 +89,6 @@ class PrinterTemperatureMCU:
         self.debug_read_cmd = _mcu.lookup_query_command(
             "debug_read order=%c addr=%u", "debug_result val=%u"
         )
-
-        # Obtain mcu information
-        _mcu = self._mcu = self.mcu_adc.get_mcu()
         self.mcu_type = _mcu.get_constants().get("MCU", "")
         # Run MCU specific configuration
         cfg_funcs = [
@@ -139,29 +136,6 @@ class PrinterTemperatureMCU:
             maxval=max(adc_range),
             range_check_count=self._danger_check_count,
         )
-
-    def setup_callback(self, temperature_callback):
-        self.temperature_callback = temperature_callback
-
-    def get_report_time_delta(self):
-        return REPORT_TIME
-
-    def adc_callback(self, read_time, read_value):
-        temp = self.base_temperature + read_value * self.slope
-        self.temperature_callback(read_time + SAMPLE_COUNT * SAMPLE_TIME, temp)
-
-    def setup_minmax(self, min_temp, max_temp):
-        self.min_temp = min_temp
-        self.max_temp = max_temp
-
-    def calc_adc(self, temp):
-        return (temp - self.base_temperature) / self.slope
-
-    def calc_base(self, temp, adc):
-        return temp - adc * self.slope
-
-    def _mcu_identify(self):
-        self._build_config()
 
     def config_unknown(self):
         raise self.printer.config_error(
